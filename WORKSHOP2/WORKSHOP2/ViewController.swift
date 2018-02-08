8ec5c683090ad2af31f25d9155d4d8454ed3de25
admin <celine.meme@eemi.com>
2018-02-08 16:15:55 +0100

    Base Inscription

--- WORKSHOP2/WORKSHOP2/ViewController.swift
+++ WORKSHOP2/WORKSHOP2/ViewController.swift
@@ -20,6 +20,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func onClickRedirectWebSite(_ sender: Any) {
+        
+        UIApplication.shared.open(URL(string: "https://www.ffhado.fr")!, options: [:], completionHandler: nil)
+        
+    }
+    
 
 }
 
